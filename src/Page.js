431d1a1c3df6a5d7e59d6a778bd7d6004975b60e
Nirojan21 <nirojan.yoga@gmail.com>
2023-10-30 19:26:28 +0530

    deploy

--- src/Page.js
+++ src/Page.js
@@ -1,19 +1,19 @@
 import React from 'react'
-import { Navigate, Route, Routes } from "react-router";
+import { Route, Routes } from "react-router";
 import Home from './pages/Home'
 import NER from './pages/NER';
 import OCR from './pages/OCR';
 import Sentiment from './pages/Sentiment';
-import Predicition from './pages/Predicition';
+import Prediction from './pages/Prediction';
 
 export default function Page() {
   return (
     <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="/ner-pricing" element={<NER />} />
         <Route path="/ocr-pricing" element={<OCR />} />
         <Route path="/sentiment_analysis-pricing" element={<Sentiment />} />
-        <Route path="/predictive_text-pricing" element={<Predicition />} />
+        <Route path="/predictive_text-pricing" element={<Prediction />} />
+        <Route path="/" element={<Home />} />
     </Routes>
   )
 }
